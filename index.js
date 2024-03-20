3a638401f45ab5e34d7318a7e14965ed31ae2a55
FEM 1 <fem1@weboccult.com>
2024-03-20 15:18:33 +0530

    Initial Commmit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.all('/', (req, res) => {
+    const {body, method} = req;
+    if('responseCode' in body){
+        const message = body.errorMessage || `Response code is ${body.responseCode}`;
+        res.status(body.responseCode).json({method, message});
+        return;
+    }
+    res.status(200).json({method, message: 'Response code is 200', status: 200});
+})
+
+
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+})
